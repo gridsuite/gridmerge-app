a0680bff5f6e6d1ee0e26afa5fba011cfe4ddaf1
Mohamed Hedi Khefacha <mohamed.hedi@rte-france.com>
2020-08-31 15:57:06 +0200

    resolve changes requested
    Signed-off-by: Mohamed Hedi Khefacha <mohamed.hedi@rte-france.com>

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
-    createProxyMiddleware('http://localhost:5001/api/study-server', {
+    createProxyMiddleware('http://localhost:5001/api/merge-server', {
         pathRewrite: { '^/api/api-server/': '/' }
     })
   );
