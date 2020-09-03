bc29ed3973ec161ff42e45290ba22187ee510c67
Mohamed Hedi Khefacha <mohamed.hedi@rte-france.com>
2020-09-03 16:38:25 +0200

    edit files

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,12 +2,12 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(
         createProxyMiddleware('http://localhost:5020/api/merge-orchestrator-server', {
-            pathRewrite: { '^/api/api-server/': '/' }
+            pathRewrite: { '^/api/merge-orchestrator-server/': '/' }
         })
     );
     app.use(
-        createProxyMiddleware('http://localhost:5020/ws/merge-orchestrator-server', {
-            pathRewrite: { '^/ws/api-server/': '/' },
+        createProxyMiddleware('http://localhost:5002/ws/merge-orchestrator-server', {
+            pathRewrite: { '^/ws/merge-orchestrator-server': '/' },
             ws: true,
         })
     );
