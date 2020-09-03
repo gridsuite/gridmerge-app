29343fff770cc94650dc5c677f84ea16db78de0f
Mohamed Hedi Khefacha <mohamed.hedi@rte-france.com>
2020-09-03 14:42:41 +0200

    Add websocket in proxy file
    Signed-off-by: Mohamed Hedi Khefacha <mohamed.hedi@rte-france.com>

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,25 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
-  app.use(
-    createProxyMiddleware('http://localhost:5001/api/merge-orchestrator-server', {
-        pathRewrite: { '^/api/api-server/': '/' }
-    })
-  );
+    app.use(
+        createProxyMiddleware('http://localhost:8080/api/merge-orchestrator-server', {
+            pathRewrite: { '^/api/api-server/': '/' }
+        })
+    );
+    app.use(
+        createProxyMiddleware('http://localhost:8080/ws/merge-orchestrator-server', {
+            pathRewrite: { '^/ws/api-server/': '/' },
+            ws: true,
+        })
+    );
+    app.use(
+        createProxyMiddleware('http://localhost:9000/api/gateway', {
+            pathRewrite: { '^/api/gateway/': '/' },
+        })
+    );
+    app.use(
+        createProxyMiddleware('http://localhost:9000/ws/gateway', {
+            pathRewrite: { '^/ws/gateway/': '/' },
+            ws: true,
+        })
+    );
 };
