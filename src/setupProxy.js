1245dd0e7b351794e17aff104bb62b468f6d351d
Mohamed Hedi <khefacha.hedi@gmail.com>
2020-09-03 18:28:24 +0200

    Add fix backend addresses in proxy and docker image, sync base regexp with newer template (#3)
    Signed-off-by: Mohamed Hedi Khefacha <mohamed.hedi@rte-france.com>

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,25 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
-  app.use(
-    createProxyMiddleware('http://localhost:5001/api/merge-orchestrator-server', {
-        pathRewrite: { '^/api/api-server/': '/' }
-    })
-  );
+    app.use(
+        createProxyMiddleware('http://localhost:5020/api/merge-orchestrator-server', {
+            pathRewrite: { '^/api/merge-orchestrator-server/': '/' }
+        })
+    );
+    app.use(
+        createProxyMiddleware('http://localhost:5002/ws/merge-notification-server', {
+            pathRewrite: { '^/ws/merge-notification-server': '/' },
+            ws: true,
+        })
+    );
+    app.use(
+        createProxyMiddleware('http://localhost:9000/api/gateway', {
+            pathRewrite: { '^/api/gateway/': '/' },
+        })
+    );
+    app.use(
+        createProxyMiddleware('http://localhost:9000/ws/gateway', {
+            pathRewrite: { '^/ws/gateway/': '/' },
+            ws: true,
+        })
+    );
 };
