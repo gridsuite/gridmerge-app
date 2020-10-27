dcf01b53d8bfc7c6728550ac8e8974eaec35f7bb
Mohamed Hedi Khefacha <mohamed.hedi@rte-france.com>
2020-10-27 13:54:32 +0100

    Add stepper
    Signed-off-by: Mohamed Hedi Khefacha <mohamed.hedi@rte-france.com>

--- src/components/stepper.js
+++ src/components/stepper.js
@@ -4,36 +4,51 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
 import IconButton from '@material-ui/core/IconButton';
+import Grid from '@material-ui/core/Grid';
+import WarningOutlinedIcon from '@material-ui/icons/WarningOutlined';
 import { FormattedMessage } from 'react-intl';
 import GetAppIcon from '@material-ui/icons/GetApp';
-import { getExportMergeUrl } from '../utils/api';
+import { getExportMergeUrl, getIgmStatus, IgmStatus } from '../utils/api';
 import PropTypes from 'prop-types';
 
 const useStyles = makeStyles((theme) => ({
-    download: {
-        display: 'grid',
-        float: 'right',
+    stepperContainer: {
         position: 'absolute',
-        bottom: '0',
-        padding: '10px 10px 10px 10px',
-        backgroundColor: theme.palette.background.paper,
-    },
-    downloadLabel: {
-        fontSize: '15px',
+        bottom: 0,
     },
     downloadLabelDisabled: {
-        color: '#ffffff4d',
+        color: '#b5b5b5',
+    },
+    downloadBtn: {
+        textAlign: 'center',
+        backgroundColor: theme.palette.background.paper,
     },
 }));
 
-const DownloadButton = (props) => {
+const CustomStepper = withStyles({
+    root: {
+        padding: '53px 25px 35px 25px;',
+    },
+})(Stepper);
+
+const CustomStepLabel = withStyles({
+    label: {
+        color: 'red',
+    },
+})(StepLabel);
+
+const StepperWithStatus = (props) => {
     const DownloadIframe = 'downloadIframe';
 
     const classes = useStyles();
+    const [steps, setSteps] = useState([]);
 
     const handleClickExport = () => {
         console.info('Downloading merge ' + props.merge.process + '...');
@@ -51,36 +66,146 @@ const DownloadButton = (props) => {
         );
     };
 
-    const disabled = !props.merge;
+    /**
+     * Check if all countries status is equal
+     * @param arr
+     * @returns {boolean}
+     */
+    const allEqual = (arr) => arr.every((v) => v === arr[0]);
+
+    /**
+     * Get status if all countries status is equal
+     * @returns {string}
+     */
+    const getStatus = () => {
+        if (allEqual(steps)) {
+            switch (steps[0]) {
+                case IgmStatus.ABSENT:
+                    return IgmStatus.ABSENT;
+
+                case IgmStatus.AVAILABLE:
+                    return IgmStatus.AVAILABLE;
+
+                case IgmStatus.INVALID:
+                    return IgmStatus.INVALID;
+
+                case IgmStatus.VALID:
+                    return IgmStatus.VALID;
+
+                case IgmStatus.MERGED:
+                    return IgmStatus.MERGED;
+
+                default:
+                    return IgmStatus.ABSENT;
+            }
+        }
+    };
+
+    /**
+     * Check if the available step is active
+     * @returns {boolean}
+     */
+    const isAvailableStep = () => {
+        if (allEqual(steps)) {
+            return getStatus() === IgmStatus.AVAILABLE ||
+                getStatus() === IgmStatus.VALID ||
+                getStatus() === IgmStatus.MERGED
+                ? true
+                : false;
+        }
+    };
+
+    /**
+     * Check if the valid step is active
+     * @returns {boolean}
+     */
+    const isValidStep = () => {
+        if (allEqual(steps)) {
+            return getStatus() === IgmStatus.VALID ||
+                getStatus() === IgmStatus.MERGED
+                ? true
+                : false;
+        }
+    };
+
+    /**
+     * Check if merged step is active
+     * @returns {boolean}
+     */
+    const isMergedStep = () => {
+        if (allEqual(steps)) {
+            return getStatus() === IgmStatus.MERGED ? true : false;
+        }
+    };
+
+    useEffect(() => {
+        const allEqual = (arr) => arr.every((v) => v === arr[0]);
+        const steps = props.tsos.map((tso) => {
+            if (tso) {
+                const status = getIgmStatus(tso, props.merge);
+                if (status) {
+                    return status;
+                }
+            }
+        });
+        setSteps(steps);
+    }, [props.merge]);
 
     return (
-        <div className={classes.download}>
-            <IconButton
-                aria-label="download"
-                onClick={handleClickExport}
-                disabled={disabled}
-            >
-                <GetAppIcon fontSize="large" />
-            </IconButton>
-            <span className={disabled ? classes.downloadLabelDisabled : ''}>
-                <FormattedMessage id="download" />
-                <span> CGM</span>
-            </span>
-            <iframe
-                title="download"
-                id={DownloadIframe}
-                name={DownloadIframe}
-                style={{ visibility: 'hidden', width: 0, height: 0 }}
-            />
-        </div>
+        <Grid container direction="row" className={classes.stepperContainer}>
+            <Grid item xs={12} md={10}>
+                <CustomStepper activeStep={isAvailableStep ? 0 : -1}>
+                    <Step active={isAvailableStep()}>
+                        <StepLabel>
+                            <FormattedMessage id="available" />
+                        </StepLabel>
+                    </Step>
+                    <Step active={isValidStep()}>
+                        <StepLabel>
+                            <FormattedMessage id="valid" />
+                        </StepLabel>
+                    </Step>
+                    <Step active={isMergedStep() === true}>
+                        <StepLabel>
+                            <FormattedMessage id="merged" />
+                        </StepLabel>
+                    </Step>
+                </CustomStepper>
+            </Grid>
+            <Grid item xs={12} md={2} className={classes.downloadBtn}>
+                <IconButton
+                    aria-label="download"
+                    style={{ width: '50px' }}
+                    onClick={handleClickExport}
+                    disabled={!isMergedStep()}
+                >
+                    <GetAppIcon fontSize="large" />
+                </IconButton>
+                <span
+                    className={
+                        !isMergedStep() ? classes.downloadLabelDisabled : ''
+                    }
+                    style={{ display: 'block' }}
+                >
+                    <FormattedMessage id="download" />
+                    <span> CGM</span>
+                </span>
+                <iframe
+                    title="download"
+                    id={DownloadIframe}
+                    name={DownloadIframe}
+                    style={{ visibility: 'hidden', width: 0, height: 0 }}
+                />
+            </Grid>
+        </Grid>
     );
 };
 
-DownloadButton.propTypes = {
+StepperWithStatus.propTypes = {
     merge: PropTypes.shape({
         process: PropTypes.string.isRequired,
         date: PropTypes.string.isRequired,
     }),
 };
 
-export default DownloadButton;
+export default StepperWithStatus;
