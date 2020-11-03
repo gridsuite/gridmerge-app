107f4db5636fae7c50f3d25b837653cfd8acafb7
FranckLecuyer <47824306+FranckLecuyer@users.noreply.github.com>
2020-11-03 22:16:47 +0100

    Deactivate the default context menu (right click) (#33)
    Co-authored-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- src/components/app.js
+++ src/components/app.js
@@ -111,6 +111,12 @@ const App = () => {
     );
 
     useEffect(() => {
+        document.addEventListener('contextmenu', (event) => {
+            event.preventDefault();
+        });
+    });
+
+    useEffect(() => {
         initializeAuthenticationProd(
             dispatch,
             initialMatchSilentRenewCallbackUrl != null,
